b084a182bcab97c85d6b495a4c444ed3067edb8a
ysBach <dbstn95@gmail.com>
2020-12-06 12:51:19 +0900

    update during ryugu observation at BOAO

--- set_retarder_angle.py
+++ set_retarder_angle.py
@@ -65,6 +65,10 @@ if __name__=="__main__":
 		# print(dangle)
 		time.sleep(0.5)
 		sys.exit(angle)
+	
+	#by Sunho Jin (BOAO troubleshooting)
+	#go_to_angle(0)
+	#time.sleep(10)
 
 	go_to_angle(angle_to)
 	# print(angle_to)
@@ -78,21 +82,23 @@ if __name__=="__main__":
 		angle_now = get_angle()
 		dangle = abs(angle_now - angle_to)
 		# print(angle_now, angle_to)
+		time.sleep(0.3)
+		sys.exit(angle)
 
-		if (dangle < ACCURACY) or (dangle > (360 - ACCURACY)):
+		#if (dangle < ACCURACY) or (dangle > (360 - ACCURACY)):
 			# print(dangle)
-			time.sleep(0.3)
-			sys.exit(angle)
+		#	time.sleep(0.3)
+		#	sys.exit(angle)
 
-		time_now = time.time()
-		dtime = time_now - time_start
+		#time_now = time.time()
+		#dtime = time_now - time_start
 
 		# If not at the desired place for 8 sec, 
 		# (1) reset angle to 0
 		# (2) reset the starting time.
-		if dtime > 8:
-			go_to_angle(0)
-			time.sleep(5)
-			time_start = time.time()
-			go_to_angle(angle_to)
+		#if dtime > 15:
+		#	go_to_angle(-80)
+		#	time.sleep(10)
+		#	time_start = time.time()
+		#	go_to_angle(angle_to)
 
