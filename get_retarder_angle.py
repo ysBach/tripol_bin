7e524e400d25b9dd32ce26b69685f0e02da6d3f5
tripol002 <dbstn95@gmail.com>
2020-12-06 13:11:07 +0900

    make get_angle as function

--- get_retarder_angle.py
+++ get_retarder_angle.py
@@ -1,27 +1,34 @@
 #import requests
 
-#url='http://127.0.0.1:8000/'
+# url='http://127.0.0.1:8000/'
 #r = requests.get(url + "get/angle/to")
-#print(r.text)
+# print(r.text)
 
 
 from http.client import HTTPConnection
 import time
 import sys
 
-if __name__=='__main__':
-	try:
-		start = time.time()
-		conn = HTTPConnection("localhost", port=8000)
-		conn.close()
-		conn = HTTPConnection("localhost", port=8000)
-		conn.request("GET", "/get/angle/to")
-		r = conn.getresponse()
-		angle = float(r.read())
-		if time.time() - start > 20:
-			sys.stderr.write("TIMEOUT")
-			sys.exit()
+__all__ = ["get_angle"]
 
-	except ConnectionRefusedError:
-		sys.exit()
-	sys.exit(angle)
+
+def get_angle():
+    try:
+        start = time.time()
+        conn = HTTPConnection("localhost", port=8000)
+        conn.close()
+        conn = HTTPConnection("localhost", port=8000)
+        conn.request("GET", "/get/angle/to")
+        r = conn.getresponse()
+        angle = float(r.read())
+        if time.time() - start > 20:
+            sys.stderr.write("TIMEOUT")
+            sys.exit()
+
+    except ConnectionRefusedError:
+        sys.exit()
+    sys.exit(angle)
+
+
+if __name__ == '__main__':
+    get_angle()
